0407bb2ac28066dc0a2b1852594317fc1ea381c1
Johannes <johannes@orager.dk>
2023-03-13 14:57:22 +0100

    workon random array

--- src/lib.rs
+++ src/lib.rs
@@ -5,9 +5,18 @@ pub use bubblesort::bubblesort;
 pub use quicksort::quicksort;
 
 use rand::Rng;
-use std::ops::RangeBounds;
+use std::{ops::Range, ops::RangeBounds, ops::Bound};
+pub fn generate_random_array(salt: i8, range: Range<i32>) -> [u8; 128]{
+    match (range.start_bound(), range.end_bound()){
+        (Bound::Included(start), Bound::Excluded(end)) => { // TODO: should only be included
+            if start >= end {
+                panic!("Start bound must be less than end bound"); // 3..0
+            }
+        },
+        _=> panic!("Unsupported rangebound") // marcro: unimplemented!() (Error if chase is not covered)
 
-pub fn generate_random_array<R: RangeBounds<i32>>(salt: i8, range: R) -> [u8; 128]{
+    }
+    
     let mut arr = [0u8; 128];
     let mut rng = rand::thread_rng();
     rng.fill(&mut arr);  
