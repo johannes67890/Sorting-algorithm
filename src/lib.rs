e3b58b470438f948694ecd95d63876996a96e72a
Johannes <johannes@orager.dk>
2023-03-16 12:39:48 +0100

    merge sort workon

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 mod bubble_sort;
 mod quick_sort;
+mod insertion_sort;
+mod merge_sort;
 
 pub use bubble_sort::bubblesort;
 pub use quick_sort::quicksort;
+pub use insertion_sort::insertionsort;
+pub use merge_sort::mergesort;
 
 use rand::Rng;
 use std::{ops::Range, ops::RangeBounds, ops::Bound};
@@ -28,7 +32,18 @@ pub fn generate_random_array(salt: i8, range: Range<i32>) -> [u8; 128]{
 #[cfg(test)]
 mod tests {
     use super::*;
-    
+
+    /// Unit tests for generate_random_array
+    #[test]
+    fn test_generate_random_array(){
+        let arr = generate_random_array(0, 0..128);
+        assert_eq!(arr.len(), 128);
+    }
+
+    ///
+    /// Test sorting algorithms
+    /// 
+
     #[test]
     fn test_bubble_sort() {
         let mut arr = [6, 2, 4, 1, 9, -2, 5];
@@ -39,6 +54,18 @@ mod tests {
     fn test_quick_sort() {
         let mut arr = [6, 2, 4, 1, 9, -2, 5];
         quicksort(&mut arr);
+        assert_eq!(arr, [-2, 1, 2, 4, 5, 6, 9]);
+    }
+    #[test]
+    fn test_insertion_sort(){
+        let mut arr = [6, 2, 4, 1, 9, -2, 5];
+        insertionsort(&mut arr);
+        assert_eq!(arr, [-2, 1, 2, 4, 5, 6, 9]);
+    }
+    #[test]
+    fn test_merge_sort(){
+        let mut arr = [6, 2, 4, 1, 9, -2, 5];
+        mergesort(&mut arr);
         assert_eq!(arr, [-2, 1, 2, 4, 5, 6, 9]);
     }
 }
--- src/merge_sort.rs
+++ src/merge_sort.rs
@@ -1,21 +1,57 @@
-pub fn mergesort<T: Ord>(arr: T) {
-    let left: isize = 0;
-    let right: isize = (array.len() - 1) as isize; // Highest index of array
+use std::vec;
 
-    if(left >= right) {
+
+pub fn mergesort<T: Ord>(arr: &mut [T]) {
+    sort(arr, 0, (arr.len() - 1));
+}
+
+fn sort<T: Ord>(arr: &mut [T], left: usize, right: usize) {
+
+    if(left >= right) { // recursive 
         return;
     }
-    let mid: isize = (left + right) / 2; // Middle index of array
+    let mid: usize = (left + right) / 2; // Middle index of array
 
     sort(arr, left, mid);
     sort(arr, mid + 1, right);
     merge(arr, left, mid, right);
 }
 
-fn sort<T: Ord>(arr: T, left: isize, right: isize) {
+fn merge<T: Ord>(arr: &mut [T], left: usize, mid: usize, right: usize) {
 
-}
 
-fn merge<T: Ord>(arr: T, left: isize, mid: isize, right: isize) {
+    let n1 = mid - left + 1;
+    let n2 = right - mid;
+
+    let left_arr: Vec<&T> = (0..n1).into_iter().map(|i| &arr[left+i] ).collect();
+
+   let right_arr: Vec<&T> = (0..n2).into_iter().map(|i| &arr[mid + 1 + i] ).collect();
+
+    let mut i:usize = 0;
+    let mut j:usize = 0;
+    let mut k:usize = left;
+
+    while i < n1 && j < n2 {
+        if left_arr[i] <= right_arr[j] {
+            arr[k] = left_arr[i];
+            i += 1;
+        } else {
+            arr[k] = right_arr[j];
+            j += 1;
+        }
+        k += 1;
+    }
+
+    while i < n1 {
+        arr[k] = left_arr[i];
+        i += 1;
+        k += 1;
+    }
+
+    while j < n2 {
+        arr[k] = right_arr[j];
+        j += 1;
+        k += 1;
+    }  
 
 }
